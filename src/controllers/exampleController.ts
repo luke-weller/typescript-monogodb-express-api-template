3cbf5b680380aae6545e5e8be75aeac640b90fc9
luke-weller <wellerluke@hotmail.com>
2024-07-19 21:46:10 -0400

    naming bug

--- src/controllers/exampleController.ts
+++ src/controllers/exampleController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import ExampleModel from "../models/exampleModel";
+import { ExampleModel } from "../models/exampleModel";
 
 // Create a new example
 export const createExample = async (req: Request, res: Response) => {
